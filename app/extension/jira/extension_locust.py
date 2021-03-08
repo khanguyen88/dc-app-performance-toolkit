2074f42d5e2b5032523e390a1523f6a997688831
Kha Nguyen <darthhanter@gmail.com>
2021-03-08 17:23:47 +0700

    add app specific actions

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -1,28 +1,127 @@
-import re
-from locustio.common_utils import init_logger, jira_measure
+from locustio.common_utils import raise_if_login_failed, fetch_by_re, RESOURCE_HEADERS, timestamp_int, \
+    generate_random_string, TEXT_HEADERS, init_logger, jira_measure
+from locustio.jira.requests_params import EditIssue, AddComment
 
 logger = init_logger(app_type='jira')
 
 
-@jira_measure("locust_app_specific_action")
 def app_specific_action(locust):
-    r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
-    content = r.content.decode('utf-8')   # decode response content
+    edit_issue_to_trigger_outbound_notification(locust)
+    add_comment_trigger_outbound_notification(locust)
 
-    token_pattern_example = '"token":"(.+?)"'
-    id_pattern_example = '"id":"(.+?)"'
-    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
-    id = re.findall(id_pattern_example, content)    # get ID from response using regexp
 
-    logger.locust_info(f'token: {token}, id: {id}')  # log info for debug when verbose is true in jira.yml file
-    if 'assertion string' not in content:
-        logger.error(f"'assertion string' was not found in {content}")
-    assert 'assertion string' in content  # assert specific string in response content
+@jira_measure("locust_edit_issue_to_trigger_outbound_notification")
+def edit_issue_to_trigger_outbound_notification(locust):
+    params = EditIssue()
+    issue_id = 2013911
+    issue_key = "DEV-5"
+    project_key = "DEV"
 
-    body = {"id": id, "token": token}  # include parsed variables to POST request body
-    headers = {'content-type': 'application/json'}
-    r = locust.post('/app/post_endpoint', body, headers, catch_response=True)  # call app-specific POST endpoint
-    content = r.content.decode('utf-8')
-    if 'assertion string after successful POST request' not in content:
-        logger.error(f"'assertion string after successful POST request' was not found in {content}")
-    assert 'assertion string after successful POST request' in content  # assertion after POST request
+    @jira_measure('locust_edit_issue_to_trigger_outbound_notification:open_editor')
+    def edit_issue_open_editor():
+        raise_if_login_failed(locust)
+        r = locust.get(f'/secure/EditIssue!default.jspa?id={issue_id}', catch_response=True)
+        content = r.content.decode('utf-8')
+
+        issue_type = fetch_by_re(params.issue_type_pattern, content)
+        atl_token = fetch_by_re(params.atl_token_pattern, content)
+        priority = fetch_by_re(params.issue_priority_pattern, content, group_no=2)
+        assignee = fetch_by_re(params.issue_assigneee_reporter_pattern, content, group_no=2)
+        reporter = fetch_by_re(params.issue_reporter_pattern, content)
+
+        if not (f' Edit Issue:  [{issue_key}]' in content):
+            logger.error(f'{params.err_message_issue_not_found} - {issue_id}, {issue_key}: {content}')
+        assert f' Edit Issue:  [{issue_key}]' in content, \
+            params.err_message_issue_not_found
+        logger.locust_info(f"{params.action_name}: Editing issue {issue_key}")
+
+        locust.post('/rest/webResources/1.0/resources', json=params.resources_body.get("705"),
+                    headers=RESOURCE_HEADERS, catch_response=True)
+        locust.post('/rest/webResources/1.0/resources', json=params.resources_body.get("710"),
+                    headers=RESOURCE_HEADERS, catch_response=True)
+        locust.post('/rest/webResources/1.0/resources', json=params.resources_body.get("720"),
+                    headers=RESOURCE_HEADERS, catch_response=True)
+        locust.get(f'/rest/internal/2/user/mention/search?issueKey={issue_key}'
+                   f'&projectKey={project_key}&maxResults=10&_={timestamp_int()}', catch_response=True)
+
+        edit_body = f'id={issue_id}&summary="DC-TestIssue{generate_random_string(15)}"&issueType={issue_type}&priority={priority}' \
+                    f'&dueDate=""&assignee={assignee}&reporter={reporter}&environment=""' \
+                    f'&description={generate_random_string(500)}&timetracking_originalestimate=""' \
+                    f'&timetracking_remainingestimate=""&isCreateIssue=""&hasWorkStarted=""&dnd-dropzone=""' \
+                    f'&comment=""&commentLevel=""&atl_token={atl_token}&Update=Update'
+        locust.session_data_storage['edit_issue_body'] = edit_body
+        locust.session_data_storage['atl_token'] = atl_token
+
+    edit_issue_open_editor()
+
+    @jira_measure('locust_edit_issue_to_trigger_outbound_notification:save_edit')
+    def edit_issue_save_edit():
+        raise_if_login_failed(locust)
+        r = locust.post(f'/secure/EditIssue.jspa?atl_token={locust.session_data_storage["atl_token"]}',
+                        params=locust.session_data_storage['edit_issue_body'],
+                        headers=TEXT_HEADERS, catch_response=True)
+        content = r.content.decode('utf-8')
+        if not (f'[{issue_key}]' in content):
+            logger.error(f'Could not save edited page: {content}')
+        assert f'[{issue_key}]' in content, 'Could not save edited page'
+
+        locust.get(f'/browse/{issue_key}', catch_response=True)
+        locust.post('/rest/webResources/1.0/resources', json=params.resources_body.get("740"),
+                    headers=RESOURCE_HEADERS, catch_response=True)
+        locust.post('/rest/webResources/1.0/resources', json=params.resources_body.get("745"),
+                    headers=RESOURCE_HEADERS, catch_response=True)
+        locust.post('/rest/webResources/1.0/resources', json=params.resources_body.get("765"),
+                    headers=RESOURCE_HEADERS, catch_response=True)
+        locust.get(f'/secure/AjaxIssueEditAction!default.jspa?decorator=none&issueId='
+                   f'{issue_id}&_={timestamp_int()}', catch_response=True)
+        locust.post('/rest/webResources/1.0/resources', json=params.resources_body.get("775"),
+                    headers=RESOURCE_HEADERS, catch_response=True)
+        locust.client.put(f'/rest/projects/1.0/project/{project_key}/lastVisited', params.last_visited_body,
+                          catch_response=True)
+
+    edit_issue_save_edit()
+
+
+@jira_measure("locust_add_comment_trigger_outbound_notification")
+def add_comment_trigger_outbound_notification(locust):
+    params = AddComment()
+    issue_id = 2013912
+    issue_key = "DEV-6"
+    project_key = "DEV"
+
+    @jira_measure('locust_add_comment_trigger_outbound_notification:open_comment')
+    def add_comment_open_comment():
+        raise_if_login_failed(locust)
+        r = locust.get(f'/secure/AddComment!default.jspa?id={issue_id}', catch_response=True)
+        content = r.content.decode('utf-8')
+        token = fetch_by_re(params.atl_token_pattern, content)
+        form_token = fetch_by_re(params.form_token_pattern, content)
+        if not (f'Add Comment: {issue_key}' in content):
+            logger.error(f'Could not open comment in the {issue_key} issue: {content}')
+        assert f'Add Comment: {issue_key}' in content, 'Could not open comment in the issue'
+
+        locust.post('/rest/webResources/1.0/resources', json=params.resources_body.get("805"),
+                    headers=RESOURCE_HEADERS, catch_response=True)
+        locust.post('/rest/webResources/1.0/resources', json=params.resources_body.get("810"),
+                    headers=RESOURCE_HEADERS, catch_response=True)
+        locust.post('/rest/webResources/1.0/resources', json=params.resources_body.get("820"),
+                    headers=RESOURCE_HEADERS, catch_response=True)
+        locust.get(f'/rest/internal/2/user/mention/search?issueKey={issue_key}&projectKey={project_key}'
+                   f'&maxResults=10&_={timestamp_int()}', catch_response=True)
+        locust.session_data_storage['token'] = token
+        locust.session_data_storage['form_token'] = form_token
+    add_comment_open_comment()
+
+    @jira_measure('locust_add_comment_trigger_outbound_notification:save_comment')
+    def add_comment_save_comment():
+        raise_if_login_failed(locust)
+        r = locust.post(f'/secure/AddComment.jspa?atl_token={locust.session_data_storage["token"]}',
+                        params={"id": {issue_id}, "formToken": locust.session_data_storage["form_token"],
+                                "dnd-dropzone": None, "comment": generate_random_string(20),
+                                "commentLevel": None, "atl_token": locust.session_data_storage["token"],
+                                "Add": "Add"}, headers=TEXT_HEADERS, catch_response=True)
+        content = r.content.decode('utf-8')
+        if not (f'<meta name="ajs-issue-key" content="{issue_key}">' in content):
+            logger.error(f'Could not save comment: {content}')
+        assert f'<meta name="ajs-issue-key" content="{issue_key}">' in content, 'Could not save comment'
+    add_comment_save_comment()
--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -1,5 +1,3 @@
-import random
-
 from selenium.webdriver.common.by import By
 
 from selenium_ui.base_page import BasePage
@@ -7,18 +5,23 @@ from selenium_ui.conftest import print_timing
 from util.conf import JIRA_SETTINGS
 
 
-def app_specific_action(webdriver, datasets):
+def view_notification_page(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        issue_key = datasets['custom_issue_key']
+    project_key = "DEV"
 
-    @print_timing("selenium_app_custom_action")
-    def measure():
+    @print_timing("selenium_view_notification_page:view_root")
+    def measure_root_page():
+        @print_timing("selenium_view_notification_page:view_root")
+        def visit_root():
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/ConfigureTeams.jspa?projectKey={project_key}")
+            page.wait_until_visible((By.ID, "slack-project-config"))  # Wait for summary field visible
+        visit_root()
 
-        @print_timing("selenium_app_custom_action:view_issue")
-        def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
-        sub_measure()
-    measure()
+        @print_timing("selenium_view_notification_page:view_config")
+        def measure_config_page():
+            configure_button = page.get_elements((By.CSS_SELECTOR, "#slack-project-config button.aui-button-link"))[1]
+            configure_button.click()
+            page.wait_until_visible((By.ID, "jqlform"))  # Wait for summary field visible
+        measure_config_page()
+    measure_root_page()
+
