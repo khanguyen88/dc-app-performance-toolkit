749fd47702241216c24247fef361a8dc95592ad6
Kha Nguyen <darthhanter@gmail.com>
2021-03-12 14:02:51 +0700

    add gravatar action

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -1,5 +1,3 @@
-import random
-
 from selenium.webdriver.common.by import By
 
 from selenium_ui.base_page import BasePage
@@ -9,16 +7,19 @@ from util.conf import CONFLUENCE_SETTINGS
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_pages']:
-        app_specific_page_id = datasets['custom_page_id']
 
-    @print_timing("selenium_app_custom_action")
-    def measure():
+    @print_timing("import_gravatar")
+    def manual_import_gravatar():
+        page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/users/profile/editmyprofilepicture.action")
+        page.wait_until_visible((By.ID, "gravatar-importer-button"))  # Wait for title field visible
 
-        @print_timing("selenium_app_custom_action:view_page")
-        def sub_measure():
-            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
-            page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
-        sub_measure()
-    measure()
+        import_button = page.get_element((By.ID, "gravatar-importer-button"))
+        import_button.click()
+
+        page.wait_until_visible((By.ID, "gravatar-importer-submit-button"))
+        submit_button = page.get_element((By.ID, "gravatar-importer-submit-button"))
+        submit_button.click()
+
+        page.wait_until_invisible((By.ID, "gravatar-importer-dialog"))
+
+    manual_import_gravatar()
