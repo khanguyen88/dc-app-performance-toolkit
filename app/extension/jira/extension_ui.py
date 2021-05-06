8f8ba93e9a81d60661ca39ca00232e44374ce977
Kha Nguyen <darthhanter@gmail.com>
2021-05-06 16:11:04 +0700

    selenium action for import screen navigation

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -8,37 +8,28 @@ from selenium_ui.jira.pages.pages import Login
 from util.conf import JIRA_SETTINGS
 
 
-def app_specific_action(webdriver, datasets):
+def navigate_to_import_screen(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        issue_key = datasets['custom_issue_key']
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out action
-    #
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.set_credentials(username=username, password=password)
-    #         if login_page.is_first_login():
-    #             login_page.first_login_setup()
-    #         if login_page.is_first_login_second_page():
-    #             login_page.first_login_second_page_setup()
-    #         login_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
+    @print_timing("app_specific_user_login")
+    def app_specific_user_login():
+        login_page = Login(webdriver)
+        login_page.delete_all_cookies()
+        login_page.go_to()
+        login_page.set_credentials(username='admin', password='admin')
+        if login_page.is_first_login():
+            login_page.first_login_setup()
+        if login_page.is_first_login_second_page():
+            login_page.first_login_second_page_setup()
+        login_page.wait_for_page_loaded()
+    app_specific_user_login()
 
-    @print_timing("selenium_app_custom_action")
+    @print_timing("app_specific:import_screen_navigation")
     def measure():
-        @print_timing("selenium_app_custom_action:view_issue")
-        def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
-        sub_measure()
+        page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/ImportMpp!default.jspa")
+        page.wait_until_visible((By.ID, "project-input"))  # Wait for summary field visible
+        project_list = page.get_element((By.ID, "project-input"))
+        project_list.click()
+
     measure()
 
